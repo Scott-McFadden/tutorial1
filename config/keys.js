a8c035e06b8e74e9651801346bb1b8bae138f349
Scott McFadden <scott@mdashconsulting.com>
2018-11-07 08:23:17 -0600

    change 2

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,15 @@
 
 console.log('node env: ', process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
+    console.log ('using prod');
+
     module.exports = require('./prod');
+
+    //console.log('mongo_db uri', keys.mongo.URI);
 }
 else {
+    console.log('using dev');
     module.exports = require('./dev');
+    //console.log('mongo_db uri', keys.mongo.URI);
 }
+
